7bcbeedaf4a62b9302c31befb0dfddbe79e37c20
Deniz Efe YASAR <denoz1020@gmail.com>
2021-12-23 20:32:12 +0300

    .

---
+++ LVL/lvl1.php
@@ -0,0 +1,14 @@
+<?php 
+
+$sifre=$_POST['sifre'];
+
+if($sifre == "Kaslı_Taha123")
+{
+    echo "Tebrikler Seviyeyi Başarıyla Tamamladınız. ";
+}
+else
+{
+    echo"Şifre Yanlış Tekrar Deneyiniz";
+}
+
+?>
---
+++ LVL/lvl2.php
@@ -0,0 +1,19 @@
+<?php
+
+$sifre=$_POST['sifre'];
+
+if($sifre == "Kaçovv")
+{
+    echo "Tebrikler Seviyeyi Başarıyla Tamamladınız. ";
+}
+else
+{
+    echo"Şifre Yanlış Tekrar Deneyiniz";
+}
+
+?>
+<html>
+    <body>
+    <a href="./ctf.ai" download="">sa</a>
+    </body>
+</html>
